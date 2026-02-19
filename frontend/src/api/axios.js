cebf80a54fbf7cb6fc4f68d3857c2c84d95e7c5c
lohithkola06 <lohithkola06@gmail.com>
2026-02-20 02:58:07 +0530

    feat: Dynamically configure frontend API URL and add Render deployment for frontend static site.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // all api calls go through this
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api'
+});
 
 // slap the token on every request if we have one
 api.interceptors.request.use(config => {
